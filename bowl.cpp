06743bf9a36b6315c10f2c626ba13d01e63509e3
sufiyanfalak24-tech <sufiyanfalak24@gmail.com>
2026-02-22 15:44:24 +0500

    Add files via upload

---
+++ bowl.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "          * * *" << endl;
+    cout << "           * * *" << endl;
+    cout << "            * * *" << endl;
+    cout << "             * * *" << endl;
+    cout << "              * * *" << endl;
+    cout << "               * * *" << endl;
+    cout << "  ***********************************" << endl;
+    cout << "   * *" << endl;
+    cout << "    * *" << endl;
+    cout << "     * *" << endl;
+    cout << "      * *" << endl;
+    cout << "       * *" << endl;
+    cout << "        ***********************" << endl;
+    cout << endl;
+    cout << "  #WELCOME TO F()()D()Y APPLICATITION#" << endl;
+
+    return 0;
+}
