2c0164a609b7a0b00bfbaaad167407555bc6b36f
sufiyanfalak24-tech <sufiyanfalak24@gmail.com>
2026-02-22 15:38:41 +0500

    Add files via upload

---
+++ car.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "              _________ " << endl;
+    cout << "        ..-''          _.-'']]  -''-. " << endl;
+    cout << "  _-''              ( ) |      ]]     ' ]]''-._ " << endl;
+    cout << " /  ____       ___]]---__]]_   ''-. " << endl;
+    cout << "|  T           |       xxx|x...                |     ''-. " << endl;
+    cout << "|  |     _    |       xxx|xxxxxxxxxx==        |   _    ''-._ " << endl;
+    cout << "|j     /  \\   ) )     xxx|xxxxxxxxxx==        |  /  \\   ) )    ''-. " << endl;
+    cout << "  |__  | (  ) | |      xxx|'                  | | (  ) | |          | " << endl;
+    cout << "  |_ )|  \\/  ||_________||  \\/  ||____/ " << endl;
+    cout << "      ''()      /  /                        ( (      /  / " << endl;
+    cout << "       .-''---''_ dp                      .-''---''_ " << endl;
+    cout << "               ''                                   '' " << endl;
+
+    return 0;
+}
