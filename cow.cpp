15d6a04967261b3db519ca9eff440e04867a3cf6
sufiyanfalak24-tech <sufiyanfalak24@gmail.com>
2026-02-22 15:37:42 +0500

    Add files via upload

---
+++ cow.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "---------------------------------" << endl;
+    cout << "      o   ^__^" << endl;
+    cout << "       o  (oo)\\_______" << endl;
+    cout << "          (__)\\       )\\/\\" << endl;
+    cout << "              ||----w |" << endl;
+    cout << "              ||     ||" << endl;
+
+    cout << endl << endl; //Adding some space between tasks
+
+
+}
