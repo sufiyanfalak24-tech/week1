184de6a8a2e90bb16343323e82e190050df40ee8
sufiyanfalak24-tech <sufiyanfalak24@gmail.com>
2026-02-22 15:42:51 +0500

    Add files via upload

---
+++ pokemon.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "   ,-.                               " << endl;
+    cout << "  (\\ \\           _                " << endl;
+    cout << "    \\ \\    .._../ /_              " << endl;
+    cout << "     \\ \\  /        /  /              " << endl;
+    cout << "      \\ /O    O  !  /                " << endl;
+    cout << "       ! )      O ! /        / \\     " << endl;
+    cout << "       !   - -     ;       /   \\_    " << endl;
+    cout << "       :  --      : ;     /  >  !    " << endl;
+    cout << "      /  _      /  ;    /  _  /      " << endl;
+    cout << "     (  /      (  /  : ;  <_/        " << endl;
+    cout << "      !        !      ; /            " << endl;
+    cout << "       >      !      /               " << endl;
+    cout << "      (_,- _  >  .  /                " << endl;
+    cout << "          (_,'      /                " << endl;
+
+    return
